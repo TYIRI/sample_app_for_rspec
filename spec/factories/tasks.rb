70ec5f5580852c01e80f87aaeacf74a1b72f406c
TYIRI <TYIRI@MacBook.local>
2020-12-14 20:23:58 +0900

    Fix: user.tasks.buildの代わりにFactoryBot.buildを使用

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :task do
-    title { "Test Task"}
+    sequence(:title) { |n| "Test Task #{n}" }
+    content { "aaa" }
     status { 0 }
+    deadline { 1.week.from_now }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,49 +3,32 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe "validation" do
     it "is valid with all attributes" do
-      task = FactoryBot.create(:user).tasks.build(
-        title: "Test Task",
-        status: 0
-      )
+      task = FactoryBot.build(:task)
       expect(task).to be_valid
     end
 
     it "is invalid without title" do
-      task = FactoryBot.create(:user).tasks.build(title: nil)
+      task = FactoryBot.build(:task, title: nil)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
 
     it "is invalid without status" do
-      task = FactoryBot.create(:user).tasks.build(status: nil)
+      task = FactoryBot.build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
 
     it "is invalid with a duplicate title" do
-      user = FactoryBot.create(:user)
-      user.tasks.create(
-        title: "Test Task",
-        status: 0
-      )
-      task = user.tasks.build(
-        title: "Test Task",
-        status: 0
-      )
-      task.valid?
-      expect(task.errors[:title]).to include("has already been taken")
+      task = FactoryBot.create(:task)
+      task_with_duplicated_title = FactoryBot.build(:task, title: task.title)
+      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
     it "is valid with another title" do
-      user = FactoryBot.create(:user)
-      user.tasks.create(
-        title: "First Task",
-        status: 0
-      )
-      task_with_another_title = user.tasks.build(
-        title: "Second Task",
-        status: 0
-      )
+      task = FactoryBot.create(:task)
+      task_with_another_title = FactoryBot.build(:task, title: "Second Task")
       expect(task_with_another_title).to be_valid
     end
   end
